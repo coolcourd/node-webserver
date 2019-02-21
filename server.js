cebf4c6a0f3ece952eef5d84fee1877ff3b6110c
Courd Headman <courdheadman@gmail.com>
2019-02-20 20:05:39 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require("express")
+const hbs = require("hbs")
+const fs = require("fs")
+
+var app = express()
+
+hbs.registerPartials(`${__dirname}/views/partials`)
+app.set("view engine", "hbs");
+
+app.use((req, res, next) => {
+    var now = new Date().toString()
+    var log = `${now}: ${req.method} ${req.url}`
+
+    console.log(log)
+    fs.appendFile("server.log", log + "\n", (err) => {
+        if (err) {
+            console.log("Unable to append to serve.log")
+        }
+    })
+    next()
+})
+
+// app.use((req, res, next) => {
+//     res.render("maint.hbs")
+// })
+
+app.use(express.static(`${__dirname}/public`))
+
+hbs.registerHelper("screamIt", (text) => {
+    return text.toUpperCase()
+})
+
+hbs.registerHelper("getCurrentYear", () => {
+    return new Date().getFullYear()
+})
+
+app.get('/', (req, res) => {
+    // res.send("<h1>Hello Express</h1>")
+    res.render("home.hbs", {
+        pageTitle: "Home page",
+        welcomeMessage: "Courd is Great!"
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render("about.hbs", {
+        pageTitle: "About page",
+    })
+})
+
+app.get('/bad', (req, res) => {
+    res.send({
+        error: "error handeling request"
+    })
+})
+
+app.listen(3000, () => {
+    console.log("server is up on port 3000")
+})
